08c921247148e16e22094b7ac377a053c4cb8d09
Jonas Bostoen <jonas.bostoen@hotmail.com>
2022-11-02 11:05:58 +0100

    feat: simple sync.Map benchmarks

--- sharded_map_test.go
+++ sharded_map_test.go
@@ -1,6 +1,9 @@
 package shardmap
 
-import "testing"
+import (
+	"sync"
+	"testing"
+)
 
 func TestMap(t *testing.T) {
 	sm := NewShardedMap[int, int](1000, 10, HashInt)
@@ -16,4 +19,50 @@ func TestMap(t *testing.T) {
 		}
 	}
 
+}
+
+func BenchmarkSyncMapPut(b *testing.B) {
+	var m sync.Map
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		m.Store(i, i)
+	}
+}
+
+func BenchmarkSyncMapGet(b *testing.B) {
+	var m sync.Map
+	for i := 0; i < b.N; i++ {
+		m.Store(i, i)
+	}
+	b.ResetTimer()
+	var v int
+	for i := 0; i < b.N; i++ {
+		if val, ok := m.Load(i); ok {
+			v = val.(int)
+		}
+	}
+
+	_ = v
+}
+
+func BenchmarkShardMapPut(b *testing.B) {
+	sm := NewShardedMap[int, int](b.N, 1, HashInt)
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		sm.Put(i, i)
+	}
+}
+
+func BenchmarkShardMapGet(b *testing.B) {
+	sm := NewShardedMap[int, int](b.N, 1, HashInt)
+	for i := 0; i < b.N; i++ {
+		sm.Put(i, i)
+	}
+	b.ResetTimer()
+	var v int
+	for i := 0; i < b.N; i++ {
+		v, _ = sm.Get(i)
+	}
+
+	_ = v
 }
